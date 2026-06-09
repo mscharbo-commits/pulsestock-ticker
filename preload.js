969e1594b4f541cba35784c00f549cf928a07008
mscharbo-commits <mscharbo@gmail.com>
2026-06-08 17:51:32 -0700

    Add preload.js

---
+++ preload.js
@@ -0,0 +1,10 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('pulseAPI', {
+  getSettings:  ()           => ipcRenderer.invoke('get-settings'),
+  saveSettings: (s)          => ipcRenderer.invoke('save-settings', s),
+  getQuotes:    (tickers)    => ipcRenderer.invoke('get-quotes', tickers),
+  openStock:    (ticker)     => ipcRenderer.invoke('open-stock', ticker),
+  openExternal: (url)        => ipcRenderer.invoke('open-external', url),
+  onOpenSettings: (callback) => ipcRenderer.on('open-settings', callback),
+});
